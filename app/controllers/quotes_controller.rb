ad851cd86d47bd18fcc1c4ba8caa0bbc2bb8452d
Brian Stolte <brian.stolte@gmail.com>
2015-08-03 14:47:48 +0000

    Added mobile meta tag

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,7 +6,7 @@ class QuotesController < ApplicationController
 	def create
 		@quote = Quote.create(quote_params)
 		if @quote.invalid?
-			flash[:error] = '<strong>Could not save!</strong> Only long guitar solos allowed, keep the quotes to 3 to 140 characters and the author to 3 to 50 characters.'
+			flash[:error] = '<strong>Could not save.</strong> Only long guitar solos allowed, keep the quotes to 3 to 140 characters and the author to 3 to 50 characters.'
 		end
 		redirect_to root_path
 	end
